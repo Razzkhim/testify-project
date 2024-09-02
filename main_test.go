43211a235b3ee3223db81b0530c8083bcacfc1cf
Razzkhim <rahim.20015@gmail.com>
2024-09-02 11:53:25 +0500

    precode

--- main_test.go
+++ main_test.go
@@ -7,50 +7,13 @@ import (
 	"testing"
 )
 
-func TestMainHandlerWhenOk(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	if status := responseRecorder.Code; status != http.StatusOK {
-		t.Errorf("expected status code: %d, got %d", http.StatusOK, status)
-	}
-}
-
-func TestMainHandlerWhenMissingCount(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?city=moscow", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	if status := responseRecorder.Code; status != http.StatusBadRequest {
-		t.Errorf("expected status code: %d, got %d", http.StatusBadRequest, status)
-	}
-
-	expected := `count missing`
-	if responseRecorder.Body.String() != expected {
-		t.Errorf("expected body: %s, got %s", expected, responseRecorder.Body.String())
-	}
-}
-
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=3&city=moscow", nil) // здесь нужно создать запрос к сервису
+	req := ... // здесь нужно создать запрос к сервису
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	if status := responseRecorder.Code; status != http.StatusOK {
-		t.Fatalf("expected status code: %d, got %d", http.StatusOK, status)
-	}
-
-	body := responseRecorder.Body.String()
-	list := strings.Split(body, ",")
-	if len(list) != totalCount {
-		t.Error(list)
-	}
+	// здесь нужно добавить необходимые проверки
 }
