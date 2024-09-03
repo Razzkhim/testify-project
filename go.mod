d294e9f09740209fdc501268772275db6d795ec1
Razzkhim <rahim.20015@gmail.com>
2024-09-03 09:14:52 +0500

    precode

--- go.mod
+++
@@ -1,5 +0,0 @@
-module main.go
-
-go 1.22.5
-
-require github.com/stretchr/testify v1.9.0 // indirect
--- main_test.go
+++ main_test.go
@@ -1,45 +1,10 @@
-package main
-
-import (
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"net/http"
-	"net/http/httptest"
-	"strings"
-	"testing"
-)
-
-func TestMainHandlerWhenOk(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	require.Equal(t, responseRecorder.Code, http.StatusOK)
-	assert.NotEmpty(t, responseRecorder.Body.String())
-}
-
-func TestMainHandlerWhenWrongCity(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=4&city=astana", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	assert.Equal(t, responseRecorder.Code, http.StatusBadRequest)
-	assert.Equal(t, responseRecorder.Body.String(), "wrong city value")
-}
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
+totalCount := 4
+req := ... // здесь нужно создать запрос к сервису
 
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
+responseRecorder := httptest.NewRecorder()
+handler := http.HandlerFunc(mainHandle)
+handler.ServeHTTP(responseRecorder, req)
 
-	body := responseRecorder.Body.String()
-	list := strings.Split(body, ",")
-
-	assert.Equal(t, len(list), totalCount)
+// здесь нужно добавить необходимые проверки
 }
